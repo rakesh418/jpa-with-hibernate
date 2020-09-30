8d7d5c42198db78458b0981b0d926076531d89e7
lex418 <rakesh418@gmail.com>
2020-10-01 00:33:05 +0530

    initialize first jpa hibernate repo

---
+++ jpa-hibernate/src/main/java/com/lex418/spring/basic/jpahibernate/JpaHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.lex418.spring.basic.jpahibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaHibernateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaHibernateApplication.class, args);
+    }
+
+}
---
+++ jpa-hibernate/src/test/java/com/lex418/spring/basic/jpahibernate/JpaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lex418.spring.basic.jpahibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaHibernateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
